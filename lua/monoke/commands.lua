04ecac41b4bd42128b84494245a76cd83d50f201
kesleymartins <kesley.martins@vivaldi.net>
2023-09-15 19:03:02 -0300

    add commands to open and close

--- lua/monoke/commands.lua
+++ lua/monoke/commands.lua
@@ -8,6 +8,8 @@ M.setup = function()
   local opts = { nargs=0 }
 
   api.nvim_create_user_command('MNKToggle', ui.toggle, opts)
+  api.nvim_create_user_command('MNKOpen', ui.open, opts)
+  api.nvim_create_user_command('MNKClose', ui.close, opts)
 end
 
 return M
